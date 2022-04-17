b196fcef552bbd0b9058e8032e900475d2ab9a73
Jorge <attiejorge89@gmail.com>
2022-04-16 21:51:57 -0300

    tester

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 function connectToDatabase() {
-  return mongoose.connect("mongodb://localhost:27017/TaskManager", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  return mongoose.connect(
+    "mongodb+srv://jorge:jorge1989@cluster0.yv5la.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 }
 
 mongoose.set("useCreateIndex", true);
